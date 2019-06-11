095e8b2b642dd659251b71724ee3f06527c67dec
haf001 <46520973+haf001@users.noreply.github.com>
2019-06-11 10:51:21 +0200

    Updated problem.py
    Updated the python code to include copyright notice and apply comments on where modifications were made to the original problem.py file.

--- problem.py
+++ problem.py
@@ -1,3 +1,33 @@
+#BSD 3-Clause License
+
+#Copyright (c) 2018, Alexandra K. Diem
+#All rights reserved.
+
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions are met:
+
+#* Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+
+#* Redistributions in binary form must reproduce the above copyright notice,
+# # this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#* Neither the name of the copyright holder nor the names of its
+#  contributors may be used to endorse or promote products derived from
+#  this software without specific prior written permission.
+
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from dolfin import *
 from ufl import grad as ufl_grad
 import sys
@@ -71,7 +101,7 @@ class PoroelasticProblem(object):
         self.sbcs = []
         self.fbcs = []
         self.pbcs = []
-        self.pbcs_project = []  # Extra bcs needed for pressure projection
+        self.pbcs_project = []  # Extra bcs needed for pressure projection - Modification
         self.tconditions = []
 
         # Material
@@ -211,8 +241,8 @@ class PoroelasticProblem(object):
         # theta-rule / Crank-Nicolson
         M = th*m + th_*m_n
 
-        # Fluid variational form
-        if self.N == 1:
+        # Fluid variational form   
+        if self.N == 1:          # Some modifications made here 
             A = variable(rho * self.J * inv(self.F) * self.K() * inv(self.F.T))
 
             vm = TestFunction(self.FS_M)
@@ -226,7 +256,7 @@ class PoroelasticProblem(object):
             Form += rho*q_out*vm*dx
 
         else:
-            A = [variable(rho * self.J * inv(self.F) * self.K()[i] * inv(self.F.T))
+            A = [variable(rho * self.J * inv(self.F) * self.K()[i] * inv(self.F.T)) # modification
                  for i in range(self.N)]
 
             vm = TestFunctions(self.FS_M)
